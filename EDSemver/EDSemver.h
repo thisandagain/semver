f394f6c4c098fb4605a4b3e53217e020f69d4821
Nick Brook <nrbrook@gmail.com>
2016-06-10 13:01:27 +0100

    Added header comments

--- EDSemver/EDSemver.h
+++ EDSemver/EDSemver.h
@@ -10,17 +10,64 @@
 
 @interface EDSemver : NSObject
 
+/*!
+ *  The major version number (API changes)
+ */
 @property (readonly) NSInteger major;
+/*!
+ *  The minor version (functionality added in a backwards compatible manor)
+ */
 @property (readonly) NSInteger minor;
+/*!
+ *  The patch version (bug fixes made in a backwards compatible manor)
+ */
 @property (readonly) NSInteger patch;
+/*!
+ *  The prerelease number, preceded with -, e.g. 1.2.3-alpha1
+ */
 @property (readonly, nullable) NSString *prerelease;
+/*!
+ *  The build number, preceded with +, e.g. 1.2.3+456
+ */
 @property (readonly, nullable) NSString *build;
 
+/*!
+ *  The current semver spec version
+ *
+ *  @return The spec version as a string
+ */
 + (nonnull NSString *)spec;
+/*!
+ *  Create a semver object with a version string
+ *
+ *  @param aString The version string
+ *
+ *  @return The semver object
+ */
 + (nonnull instancetype)semverWithString:(nonnull NSString *)aString;
 
+/*!
+ *  Create a semver object with a version string
+ *
+ *  @param aString The version string
+ *
+ *  @return The semver object
+ */
 - (nonnull instancetype)initWithString:(nonnull NSString *)aString;
+
+/*!
+ *  Compare semver objects
+ *
+ *  @param aVersion The version string
+ *
+ *  @return The semver object
+ */
 - (NSComparisonResult)compare:(nonnull EDSemver *)aVersion;
+/*!
+ *  Is the semver object valid?
+ *
+ *  @return YES if valid, NO otherwise
+ */
 - (BOOL)isValid;
 
 @end
